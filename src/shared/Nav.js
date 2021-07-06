e115546cb2ab5643f1d50b6331099177f3328c0f
Sakshi <choudharysakshi023@gmail.com>
2021-07-06 11:58:18 +0530

    💄 change theme

--- src/shared/Nav.js
+++ src/shared/Nav.js
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
-import Twitch from "./utlis/twitch";
-import { AiOutlineGoogle } from "react-icons/ai";
+import Logo from "../assets/logo.png";
+import { FcGoogle } from "react-icons/fc";
 
 const Nav = () => {
   return (
-    <div className="flex justify-between w-screen p-2 bg-black2 shadow-xl">
+    <div className="flex justify-between w-screen p-2 bg-primary shadow-2xl">
       <Link to="/">
-        <Twitch className="text-sm" />
+        <img src={Logo} alt="logo" className="w-14" />
       </Link>
       <Link to="/">
-        <div className="flex rounded-md px-5 py-2 mt-1 hover:opacity-75 bg-basePurple text-gray-300  font-medium text-2xl ">
-          <AiOutlineGoogle className="mr-3 mt-1" />{" "}
-          <span>Login with Google</span>
+        <div className="flex rounded-md px-5 py-3 mt-1 hover:opacity-75  bg-primarydark text-gray-300  font-medium text-2xl ">
+          <FcGoogle className="mr-3 mt-1" /> <span>Login with Google</span>
         </div>
       </Link>
     </div>
