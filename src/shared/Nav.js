11511ddd4d601b45f1ba8cae92dc7b6de494447c
Sakshi <choudharysakshi023@gmail.com>
2021-07-06 14:50:33 +0530

    🔧 setup redux

--- src/shared/Nav.js
+++ src/shared/Nav.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/logo.png";
+import Logo from "./assets/logo.png";
 import GoogleAuth from "../components/GoogleAuth";
 
 const Nav = () => {
