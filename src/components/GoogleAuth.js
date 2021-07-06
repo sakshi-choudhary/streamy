ae29080e16c0b29711ae1eb1c075dc1122e5db54
Sakshi <choudharysakshi023@gmail.com>
2021-07-06 13:45:40 +0530

    🛂 complete auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,21 +1,103 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { FiLogOut } from "react-icons/fi";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const GoogleAuth = () => {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+
+  const signedin = () =>
+    toast.success("Signed In!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
+  const signedout = () =>
+    toast.error("Logged Out!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
   useEffect(() => {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId: process.env.CLIENT_ID,
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId: `${process.env.REACT_APP_CLIENT_ID}`,
+          scope: "email",
+        })
+        .then(() => {
+          let auth = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(auth.isSignedIn.get());
+          auth.isSignedIn.listen(onAuthChange);
+        });
     });
   }, []);
 
+  const onAuthChange = () => {
+    setIsSignedIn(window.gapi.auth2.getAuthInstance().isSignedIn.get());
+  };
+
+  const _renderAuthBtn = () => {
+    if (isSignedIn === null) {
+      return <p>loading ...</p>;
+    } else if (isSignedIn) {
+      return (
+        <>
+          <FiLogOut className="mr-3 mt-1" />
+          <p>Log Out</p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <FcGoogle className="mr-3 mt-1" />
+          <p>Sign in with Google</p>
+        </>
+      );
+    }
+  };
+
+  const _handleOnClick = () => {
+    if (isSignedIn) {
+      window.gapi.auth2.getAuthInstance().signOut();
+      signedout();
+    } else {
+      window.gapi.auth2.getAuthInstance().signIn();
+      signedin();
+    }
+  };
+
   return (
     <>
-      <div className="flex rounded-md px-5 py-3 mt-1 hover:opacity-75  bg-primarydark text-gray-300  font-medium text-2xl ">
-        <FcGoogle className="mr-3 mt-1" /> <span>Login with Google</span>
-      </div>
+      <button
+        disabled={isSignedIn === null}
+        onClick={_handleOnClick}
+        className="flex rounded-md cursor-pointer px-5 py-3 mt-1 hover:opacity-75  bg-primarydark text-gray-300  font-medium text-2xl "
+      >
+        {_renderAuthBtn()}
+      </button>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 };
--- src/shared/Nav.js
+++ src/shared/Nav.js
@@ -5,10 +5,14 @@ import GoogleAuth from "../components/GoogleAuth";
 const Nav = () => {
   return (
     <div className="flex justify-between w-screen p-2 bg-primary shadow-2xl">
-      <Link to="/">
-        <img src={Logo} alt="logo" className="w-14" />
-      </Link>
-      <GoogleAuth />
+      <div>
+        <Link to="/">
+          <img src={Logo} alt="logo" className="w-14" />
+        </Link>
+      </div>
+      <div>
+        <GoogleAuth />
+      </div>
     </div>
   );
 };
